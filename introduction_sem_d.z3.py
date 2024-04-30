58341f4c28a2a16346b9e7aa6a2aec55d1c7a08d
NikolayBoyzz <123127020+NikolayBoyzz@users.noreply.github.com>
2024-04-30 20:36:40 +0300

    Add files via upload

---
+++ introduction_sem_d.z3.py
@@ -0,0 +1,13 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+numbers=[2,5,13,7,6,4]
+size = 6
+avg = 0
+whole_sum = 0
+index = 0
+while index < size:
+   whole_sum = whole_sum + numbers[index]
+   index = index + 1
+avg = whole_sum/size
+print (avg)
